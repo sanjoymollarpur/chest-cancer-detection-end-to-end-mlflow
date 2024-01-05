dc80f067d0fa53202d2bfbd318148760de75c4a9
sanjoy <sanjoymlp2014@gmail.com>
2024-01-05 12:58:09 +0530

    Data ingestion added

---
+++ research/download-file-from-google-drive.py
@@ -0,0 +1,11 @@
+import gdown
+
+url="https://drive.google.com/file/d/1sQu28HTUMBsFy_tof3iEopcrk8wvbLXf/view?usp=sharing"
+
+file_id=url.split("/")[-2]
+
+print(file_id)
+
+prefix="https://drive.google.com/uc?/export=download&id="
+
+gdown.download(prefix+file_id, "chest_ct.zip")
